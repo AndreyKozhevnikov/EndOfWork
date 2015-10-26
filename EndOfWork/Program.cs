06d0491e723689d802844a3addbb3e8d15f57655
Andrey Kozhevnikov <freazeek@mail.ru>
2015-10-26 21:51:07 +0300

    clear folders

--- EndOfWork/Program.cs
+++ EndOfWork/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,31 @@ using System.Threading.Tasks;
 namespace EndOfWork {
     class Program {
         static void Main(string[] args) {
+
+            WorkFlow f = new WorkFlow();
+
+        }
+
+
+
+
+        
+    }
+
+    public class WorkFlow {
+        public WorkFlow() {
+            ClearWorkFolders();
+        }
+        void ClearWorkFolders() {
+            List<string> listWorkFolder = new List<string>();
+            listWorkFolder.Add(@"d:\temp");
+            listWorkFolder.Add(@"d:\!Tickets\!Test");
+            listWorkFolder.Add(@"C:\Users\kozhevnikov.andrey\Downloads");
+
+            foreach (string fold in listWorkFolder) {
+                Directory.Delete(fold, true);
+                Directory.CreateDirectory(fold);
+            }
         }
     }
 }
